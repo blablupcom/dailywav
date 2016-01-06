9e04d40fb969a8f4fd2e23bef7f93dce23f930ee
blablupcom <o.volodin@yahoo.com>
2016-01-06 11:59:07 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,147 +1,90 @@
- #-*- coding: utf-8 -*-
-
-#### IMPORTS 1.0
-import os
+import requests
+from bs4 import BeautifulSoup as bs
 import re
-import scraperwiki
-import urllib2
-from datetime import datetime
-from bs4 import BeautifulSoup
-
-
-#### FUNCTIONS 1.0
-
-def validateFilename(filename):
-    filenameregex = '^[a-zA-Z0-9]+_[a-zA-Z0-9]+_[a-zA-Z0-9]+_[0-9][0-9][0-9][0-9]_[0-9QY][0-9]$'
-    dateregex = '[0-9][0-9][0-9][0-9]_[0-9QY][0-9]'
-    validName = (re.search(filenameregex, filename) != None)
-    found = re.search(dateregex, filename)
-    if not found:
-        return False
-    date = found.group(0)
-    now = datetime.now()
-    year, month = date[:4], date[5:7]
-    validYear = (2000 <= int(year) <= now.year)
-    if 'Q' in date:
-        validMonth = (month in ['Q0', 'Q1', 'Q2', 'Q3', 'Q4'])
-    elif 'Y' in date:
-        validMonth = (month in ['Y1'])
-    else:
-        try:
-            validMonth = datetime.strptime(date, "%Y_%m") < now
-        except:
-            return False
-    if all([validName, validYear, validMonth]):
-        return True
-
-
-def validateURL(url):
-    try:
-        r = urllib2.urlopen(url)
-        count = 1
-        while r.getcode() == 500 and count < 4:
-            print ("Attempt {0} - Status code: {1}. Retrying.".format(count, r.status_code))
-            count += 1
-            r = urllib2.urlopen(url)
-        sourceFilename = r.headers.get('Content-Disposition')
-
-        if sourceFilename:
-            ext = os.path.splitext(sourceFilename)[1].replace('"', '').replace(';', '').replace(' ', '')
-        else:
-            ext = os.path.splitext(url)[1]
-        validURL = r.getcode() == 200
-        validFiletype = ext.lower() in ['.csv', '.xls', '.xlsx']
-        return validURL, validFiletype
-    except:
-        print ("Error validating URL.")
-        return False, False
-
-
-def validate(filename, file_url):
-    validFilename = validateFilename(filename)
-    validURL, validFiletype = validateURL(file_url)
-    if not validFilename:
-        print filename, "*Error: Invalid filename*"
-        print file_url
-        return False
-    if not validURL:
-        print filename, "*Error: Invalid URL*"
-        print file_url
-        return False
-    if not validFiletype:
-        print filename, "*Error: Invalid filetype*"
-        print file_url
-        return False
-    return True
-
-def convert_mth_strings ( mth_string ):
-    month_numbers = {'JAN': '01', 'FEB': '02', 'MAR':'03', 'APR':'04', 'MAY':'05', 'JUN':'06', 'JUL':'07', 'AUG':'08', 'SEP':'09','OCT':'10','NOV':'11','DEC':'12' }
-    for k, v in month_numbers.items():
-        mth_string = mth_string.replace(k, v)
-    return mth_string
-
-
-#### VARIABLES 1.0
-
-entity_id = "E1202_BC_gov"
-url = "http://www.bournemouth.gov.uk/CouncilDemocratic/AboutYourCouncil/Transparency/PaymentstoSuppliers.aspx?GenericListPaymentstoSuppliers_List_GoToPage={}"
-errors = 0
-data = []
-proxy = urllib2.ProxyHandler({'http': '92.222.37.47:3128'})
-opener = urllib2.build_opener(proxy)
-urllib2.install_opener(opener)
-
-#### READ HTML 1.0
-
-
-html = urllib2.urlopen(url)
-soup = BeautifulSoup(html, "lxml")
-
-
-
-
-#### SCRAPE DATA
+from mutagen.mp3 import MP3
+from urllib import urlretrieve
+import json
+import math
 import itertools
 
-for pages in itertools.count(1):
-    n=str(pages)
-    html = urllib2.urlopen(url.format(n))
-    soup = BeautifulSoup(html, 'lxml')
-    block = soup.find('ol', attrs = {'class':'sys_itemslist'})
 
-    links = block.findAll('a')
+def scrape_page(base_url):
+        page = requests.get(base_url)
+        soup = bs(page.text, 'lxml')
+        links = soup.find('table', 'archives').find_all('a')
+        for link in links:
+            title_link = 'http://www.dailywav.com'+link['href']
+            title_page = requests.get(title_link)
+            title_soup = bs(title_page.text, 'lxml')
+            files_links = title_soup.find('div', 'view-content').find_all('a')
+            for files_link in files_links:
+                movie_link = 'http://www.dailywav.com'+files_link['href']
+                movie_name = files_link.text.strip()
+                movie_page = requests.get(movie_link)
+                movie_soup = bs(movie_page.text, 'lxml')
+                image_url = ''
+                try:
+                    image_url = movie_soup.find('div', 'coverArt2').find('img')['src']
+                except:
+                    try:
+                        image_url = movie_soup.find('div', 'content quotes').find('img')['src']
+                    except:
+                        pass
 
-    for link in links:
-        fileurl = 'http://www.bournemouth.gov.uk' + link['href']
+                nodes = movie_soup.find_all('div', 'node-content-wrapper')
+                for node in nodes:
+                    file_url = node.find('div', 'content quotes').find('a')['href']
+                    transcript = node.find('div', 'content quotes').find('p').text.strip()
+                    file_type = file_url.split('.')[-1]
+                    categories = 'Shows'
+                    # try:
+                    #     filename, headers = urlretrieve(link)
+                    #     audio = MP3(filename)
+                    #     if audio.info.length < 1:
+                    #         length = int(math.ceil(audio.info.length))
+                    #     else:
+                    #         length = int(round(audio.info.length))
+                    #     duration = length
+                    # except:
+                    #     duration = ''
+                    yield movie_link, movie_name, transcript,  file_type, file_url, categories, image_url
 
-        if '.csv' in fileurl:
-            csvMth = link.text[:3]
-            csvYr = link.text[-4:]
-            csvMth = convert_mth_strings(csvMth.upper())
-            data.append([csvYr, csvMth, fileurl])
-    if '57' in soup.find('span', 'sys_paginginfomaxrecord'):
-        break
-
-
-
-#### STORE DATA 1.0
-
-for row in data:
-    csvYr, csvMth, url = row
-    filename = entity_id + "_" + csvYr + "_" + csvMth
-    todays_date = str(datetime.now())
-    file_url = url.strip()
-
-    valid = validate(filename, file_url)
-
-    if valid == True:
-        scraperwiki.sqlite.save(unique_keys=['l'], data={"l": file_url, "f": filename, "d": todays_date })
-        print filename
-    else:
-        errors += 1
-
-if errors > 0:
-    raise Exception("%d errors occurred during scrape." % errors)
+                next_link = ''
+                try:
+                    next_link = movie_soup.find('li', 'pager-next').find('a')
+                except:
+                    pass
+                if next_link:
+                    movie_page = requests.get('http://www.dailywav.com'+next_link)
+                    movie_soup = bs(movie_page.text, 'lxml')
+                    continue
+                else:
+                    break
 
 
-#### EOF
+
+def scrape():
+   with open('data7.json', 'w') as outfile:
+        sounds = {}
+        lists = []
+        base_url = 'http://www.dailywav.com/archives/shows/'
+        s = scrape_page(base_url)
+        for l in s:
+            json_dic = {}
+            json_dic['sourceUrl']=l[0]
+            json_dic['movie name']=l[1]
+            json_dic['transcript']=l[2]
+            json_dic['fileType']=l[3]
+            json_dic['fileUrl']=l[4]
+            # json_dic['duration']=l[6]
+            json_dic['categories']=l[5]
+            json_dic['imageUrl']=l[5]
+            print json_dic['movie name']
+            lists.append(json_dic)
+        sounds['sounds'] = lists
+        json.dump(sounds, outfile, indent = 4)
+
+
+
+if __name__ == '__main__':
+    scrape()
