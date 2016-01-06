98c3605230f650fa1e38068d77f81c1481771100
blablupcom <o.volodin@yahoo.com>
2016-01-06 16:20:26 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,67 +2,179 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re
 from mutagen.mp3 import MP3
+import urllib
 from urllib import urlretrieve
 import json
 import math
 import itertools
+import wave
 
-
+ua = {'User-agent': 'Mozilla/5.0'}
+proxies = {'http': 'http://66.76.24.115:3128'}
 def scrape_page(base_url):
-        page = requests.get(base_url)
+        page = requests.get(base_url, headers= ua, proxies=proxies)
         soup = bs(page.text, 'lxml')
+
         links = soup.find('table', 'archives').find_all('a')
         for link in links:
             title_link = 'http://www.dailywav.com'+link['href']
-            title_page = requests.get(title_link)
+            title_page = requests.get(title_link, headers= ua, proxies=proxies)
             title_soup = bs(title_page.text, 'lxml')
-            files_links = title_soup.find('div', 'view-content').find_all('a')
+            files_links = title_soup.find('table', 'views-view-grid cols-3').find_all('a')
+
             for files_link in files_links:
+                print files_link['href']
                 movie_link = 'http://www.dailywav.com'+files_link['href']
                 movie_name = files_link.text.strip()
-                movie_page = requests.get(movie_link)
+                movie_page = requests.get(movie_link, headers= ua, proxies=proxies)
                 movie_soup = bs(movie_page.text, 'lxml')
-                image_url = ''
-                try:
-                    image_url = movie_soup.find('div', 'coverArt2').find('img')['src']
-                except:
-                    try:
-                        image_url = movie_soup.find('div', 'content quotes').find('img')['src']
-                    except:
-                        pass
-
-                nodes = movie_soup.find_all('div', 'node-content-wrapper')
-                for node in nodes:
-                    file_url = node.find('div', 'content quotes').find('a')['href']
-                    transcript = node.find('div', 'content quotes').find('p').text.strip()
-                    file_type = file_url.split('.')[-1]
-                    categories = 'Shows'
-                    # try:
-                    #     filename, headers = urlretrieve(link)
-                    #     audio = MP3(filename)
-                    #     if audio.info.length < 1:
-                    #         length = int(math.ceil(audio.info.length))
-                    #     else:
-                    #         length = int(round(audio.info.length))
-                    #     duration = length
-                    # except:
-                    #     duration = ''
-                    print movie_link.encode('utf-8'), movie_name.encode('utf-8'), transcript.encode('utf-8'),  file_type.encode('utf-8'), file_url.encode('utf-8'), categories.encode('utf-8'), image_url.encode('utf-8')
-                #     yield movie_link, movie_name, transcript,  file_type, file_url, categories, image_url
-                    scraperwiki.sqlite.save(unique_keys=["fileUrl"], data={"fileUrl": file_url.encode('utf-8'), 'sourceUrl': movie_link.encode('utf-8'), 'movie name': movie_name.encode('utf-8'), 'transcript':transcript.encode('utf-8'),  'fileType': file_type.encode('utf-8'), 'categories':categories.encode('utf-8'), 'imageUrl':image_url.encode('utf-8')
-
                 next_link = ''
                 try:
                     next_link = movie_soup.find('li', 'pager-next').find('a')['href']
                 except:
                     pass
-                if next_link:
-                    movie_page = requests.get('http://www.dailywav.com'+next_link)
-                    movie_soup = bs(movie_page.text, 'lxml')
-                    continue
-                else:
-                    break
+                if not next_link:
+                    image_url = ''
+                    try:
+                        image_url = movie_soup.find('div', 'coverArt2').find('img')['src']
+                    except:
+                        try:
+                            image_url = movie_soup.find('div', 'content quotes').find('img')['src']
+                        except:
+                            pass
 
+                    nodes = movie_soup.find_all('article', 'node node-quotes node-promoted node-teaser clearfix')
+                    for node in nodes:
+
+                        file_url = node.find('div', 'content quotes').find('div', 'field field-name-field-quote-text field-type-text-long field-label-hidden').find_previous('a')['href']
+                        transcript = node.find('div', 'content quotes').find('p').text.strip()
+                        file_type = file_url.split('.')[-1]
+                        categories = 'Shows'
+                        try:
+                            opener = urllib.FancyURLopener(proxies)
+                            filename = opener.retrieve(file_url)
+                            audio = MP3(filename[0])
+                            if audio.info.length < 1:
+                                length = int(math.ceil(audio.info.length))
+                            else:
+                                length = int(round(audio.info.length))
+                            duration = length
+                        except:
+                            try:
+                                 opener = urllib.FancyURLopener(proxies)
+                                 filename, headers = opener.retrieve(file_url)
+                                 wfile = wave.open (filename, "r")
+                                 time = (1.0 * wfile.getnframes()) / wfile.getframerate()
+                                 if time < 1:
+                                     length = int(math.ceil(time))
+                                 else:
+                                     length = int(round(time))
+                                 duration = length
+                            except:
+                                 duration = ''
+                        print duration
+                        print movie_name, transcript
+                        scraperwiki.sqlite.save(unique_keys=['fileUrl'], data={"sourceUrl":movie_link, "movie name": movie_name, "transcript": transcript, "fileType": file_type, "fileUrl":file_url, "duration":duration, "categories":categories, "imageUrl":image_url})
+                        yield movie_link, movie_name, transcript,  file_type, file_url, duration, categories, image_url
+
+                else:
+                    image_url = ''
+                    try:
+                        image_url = movie_soup.find('div', 'coverArt2').find('img')['src']
+                    except:
+                        try:
+                            image_url = movie_soup.find('div', 'content quotes').find('img')['src']
+                        except:
+                            pass
+
+                    nodes = movie_soup.find_all('article', 'node node-quotes node-promoted node-teaser clearfix')
+                    for node in nodes:
+
+                        file_url = node.find('div', 'content quotes').find('div', 'field field-name-field-quote-text field-type-text-long field-label-hidden').find_previous('a')['href']
+                        transcript = node.find('div', 'content quotes').find('p').text.strip()
+                        file_type = file_url.split('.')[-1]
+                        categories = 'Shows'
+                        try:
+                            opener = urllib.FancyURLopener(proxies)
+                            filename = opener.retrieve(file_url)
+                            audio = MP3(filename[0])
+                            if audio.info.length < 1:
+                                length = int(math.ceil(audio.info.length))
+                            else:
+                                length = int(round(audio.info.length))
+                            duration = length
+                        except:
+                            try:
+                                 opener = urllib.FancyURLopener(proxies)
+                                 filename, headers = opener.retrieve(file_url)
+                                 wfile = wave.open (filename, "r")
+                                 time = (1.0 * wfile.getnframes()) / wfile.getframerate()
+                                 if time < 1:
+                                     length = int(math.ceil(time))
+                                 else:
+                                     length = int(round(time))
+                                 duration = length
+                            except:
+                                 duration = ''
+
+                        print duration
+                        print movie_name, transcript
+                        scraperwiki.sqlite.save(unique_keys=['fileUrl'], data={"sourceUrl":movie_link, "movie name": movie_name, "transcript": transcript, "fileType": file_type, "fileUrl":file_url, "duration":duration, "categories":categories, "imageUrl":image_url})
+                        yield movie_link, movie_name, transcript,  file_type, file_url, duration, categories, image_url
+
+                    for paged in itertools.count():
+                        movie_page = requests.get(movie_link+'?page={}'.format(paged+1), headers= ua, proxies=proxies)
+                        print movie_link+'?page={}'.format(paged+1)
+                        movie_soup = bs(movie_page.text, 'lxml')
+                        find_node = ''
+                        try:
+                            find_node = movie_soup.find('div', 'content quotes')
+                        except:
+                            pass
+                        if not find_node:
+                            break
+                        image_url = ''
+                        try:
+                            image_url = movie_soup.find('div', 'coverArt2').find('img')['src']
+                        except:
+                            try:
+                                image_url = movie_soup.find('div', 'content quotes').find('img')['src']
+                            except:
+                                pass
+
+                        nodes = movie_soup.find_all('article', 'node node-quotes node-promoted node-teaser clearfix')
+                        for node in nodes:
+
+                            file_url = node.find('div', 'content quotes').find('div', 'field field-name-field-quote-text field-type-text-long field-label-hidden').find_previous('a')['href']
+                            transcript = node.find('div', 'content quotes').find('p').text.strip()
+                            file_type = file_url.split('.')[-1]
+                            categories = 'Shows'
+                            try:
+                                opener = urllib.FancyURLopener(proxies)
+                                filename = opener.retrieve(file_url)
+                                audio = MP3(filename[0])
+                                if audio.info.length < 1:
+                                    length = int(math.ceil(audio.info.length))
+                                else:
+                                    length = int(round(audio.info.length))
+                                duration = length
+                            except:
+                                try:
+                                     opener = urllib.FancyURLopener(proxies)
+                                     filename, headers = opener.retrieve(file_url)
+                                     wfile = wave.open (filename, "r")
+                                     time = (1.0 * wfile.getnframes()) / wfile.getframerate()
+                                     if time < 1:
+                                         length = int(math.ceil(time))
+                                     else:
+                                         length = int(round(time))
+                                     duration = length
+                                except:
+                                     duration = ''
+                            print duration
+                            print movie_name, transcript
+                            scraperwiki.sqlite.save(unique_keys=['fileUrl'], data={"sourceUrl":movie_link, "movie name": movie_name, "transcript": transcript, "fileType": file_type, "fileUrl":file_url, "duration":duration, "categories":categories, "imageUrl":image_url})
+                            yield movie_link, movie_name, transcript,  file_type, file_url, duration, categories, image_url
 
 
 def scrape():
@@ -78,10 +190,10 @@ def scrape():
             json_dic['transcript']=l[2]
             json_dic['fileType']=l[3]
             json_dic['fileUrl']=l[4]
-            # json_dic['duration']=l[6]
-            json_dic['categories']=l[5]
-            json_dic['imageUrl']=l[5]
-            print json_dic['movie name']
+            json_dic['duration']=l[5]
+            json_dic['categories']=l[6]
+            json_dic['imageUrl']=l[7]
+            # print json_dic['movie name']
             lists.append(json_dic)
         sounds['sounds'] = lists
         json.dump(sounds, outfile, indent = 4)
